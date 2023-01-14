7c3f87b610a9462b0031422f910a1bde36f0ecf3
naharagu <76852123+naharagu@users.noreply.github.com>
2023-01-14 10:34:12 +0100

    delete validate_args.c

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 09:49:04 by naharagu          #+#    #+#             */
-/*   Updated: 2023/01/14 09:13:25 by naharagu         ###   ########.fr       */
+/*   Updated: 2023/01/14 10:18:06 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,34 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	free_all(info);
+	return (0);
+}
+
+int	validate_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc != 5 && argc != 6)
+		return (-1);
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_atoi(argv[i]) <= 0)
+			return (-1);
+		i++;
+	}
 	return (0);
 }
